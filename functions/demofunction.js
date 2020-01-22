6dd0ebbd072c647dbf29578cdd33d6d1e3318834
Ryan Rodemoyer <r.rodemoyer@gmail.com>
2020-01-22 15:47:44 -0700

    demofunction update

--- functions/demofunction.js
+++ functions/demofunction.js
@@ -1,11 +1,6 @@
 exports.handler = async (event, context) => {
     return {
         statusCode: 200,
-        body: {
-            "values": [
-                "san francisco",
-                "kansas city"
-            ]
-        }
+        body: 'contents here'
     };
 }
