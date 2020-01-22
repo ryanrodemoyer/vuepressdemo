e1e4b3ecdc07227c1d436ad4c85a286882dafe27
Ryan Rodemoyer <r.rodemoyer@gmail.com>
2020-01-22 16:20:31 -0700

    demofunction update

--- functions/demofunction.js
+++ functions/demofunction.js
@@ -1,16 +1,28 @@
-const obj = {
-    body: 'content here',
-    res: 250,
-}
+// const obj = {
+//     body: 'content here',
+//     res: 250,
+// }
 
 const promise = new Promise((resolve, reject) => {
-
+    const usePromise = context.queryStringParameters.promise || false;
+    if (usePromise) {
+        resolve({
+            message: context.queryStringParameters.promise
+        });
+    } else {
+        reject('no promises');
+    }
 });
 
 exports.handler = async (event, context, callback) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify(obj),
-        event: JSON.stringify(event)
-    };
+    promise
+        .then(data => {
+            callback(null, JSON.stringify(data));
+        })
+        .reject(callback);
+    // return {
+    //     statusCode: 200,
+    //     body: JSON.stringify(obj),
+    //     event: JSON.stringify(event)
+    // };
 }
